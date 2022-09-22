4f90249a8dc9ab70d7084d3370f4521658d4b3d0
Michael Lipski <michael.lipski@shaw.ca>
2022-09-22 11:49:02 -0600

    updated logo and home page text

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const HomePage = () => (
     <>
-    <h1>Chef Crafted Meals Deliciously Delivered!</h1>
+    <h1>Delicious Meals at Your Fingertips</h1>
     <p>You're just two steps away from fresh, tasty meals for you and your whole family.</p>
     <div class='getstarted'>
         <input class='input-field' id='email' type='email' name='email' required placeholder='example@mail.com' />
         <input class='input-field' id='zip' type='text' name='zip' required placeholder='your zip code' />
-        <Link to='/subscriptions'><button class='submit-button'>Let's Get Fresh</button></Link>
+        <Link to='/subscriptions'><button class='submit-button'>Let's Get Started!</button></Link>
         <p>Already have an account? <Link to='/subscriptions'>Log In</Link></p>
     </div>
     </>
